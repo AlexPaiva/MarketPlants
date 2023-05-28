f56d41b98e507211d7cd3e283fcc84376dc5912a
rafarlho <rjfcarvalho@ua.pt>
2023-05-28 23:03:06 +0100

    Insert and Drop done

--- Db/DropTables.sql
+++ Db/DropTables.sql
@@ -1,4 +1,20 @@
-USE LojaDePlantas;
-DROP TABLE IF EXISTS Vendas;
-DROP TABLE IF EXISTS Plantas;
-DROP TABLE IF EXISTS Clientes;
+USE MarketPlants;
+GO
+
+DROP TABLE IF EXISTS MarketPlants.CarrinhoArtigo;
+DROP TABLE IF EXISTS MarketPlants.Carrinho;
+DROP TABLE IF EXISTS MarketPlants.Categoria;
+DROP TABLE IF EXISTS MarketPlants.Fruto;
+DROP TABLE IF EXISTS MarketPlants.Flor;
+DROP TABLE IF EXISTS MarketPlants.Planta;
+DROP TABLE IF EXISTS MarketPlants.Artigo;
+DROP TABLE IF EXISTS MarketPlants.Catalogo;
+DROP TABLE IF EXISTS MarketPlants.EntidadeRegulamentadora;
+DROP TABLE IF EXISTS MarketPlants.Vendedor;
+DROP TABLE IF EXISTS MarketPlants.Comprador;
+DROP TABLE IF EXISTS MarketPlants.Pessoa;
+DROP TABLE IF EXISTS MarketPlants.Empresa;
+DROP TABLE IF EXISTS MarketPlants.Utilizador;
+DROP TABLE IF EXISTS MarketPlants.Tipo;
+DROP SCHEMA IF EXISTS MarketPlants;
+GO
--- Db/TablesCreation.sql
+++ Db/TablesCreation.sql
@@ -5,13 +5,13 @@ Create schema MarketPlants
 Go
 
 CREATE TABLE MarketPlants.Tipo(
-	Email NVARCHAR(255) unique not null,
-	NIF	INT unique not null,
+	Email NVARCHAR(255) unique not null CHECK (Email LIKE '%@%.com'),
+	NIF	INT unique not null CHECK (NIF >= 000000000 AND NIF <= 999999999),
 	Primary Key (Email,NIF)
 );
 CREATE TABLE MarketPlants.Utilizador(
 	Username NVARCHAR(255) unique NOT NULL,
-	Senha NVARCHAR(255) NOT NULL
+	Senha NVARCHAR(255) NOT NULL,
 	Primary key (Username)
 );
 CREATE TABLE MarketPlants.Empresa(
@@ -20,8 +20,8 @@ CREATE TABLE MarketPlants.Empresa(
     Morada NVARCHAR(255),
     Nome NVARCHAR(255) unique,
     Username NVARCHAR(255) FOREIGN KEY references MarketPlants.Utilizador(Username),
-	NIFUt INT PRIMARY KEY FOREIGN KEY references MarketPlants.Tipo(nif),
-	Email NVARCHAR(255) FOREIGN KEY references MarketPlants.Tipo(email),
+	NIF INT PRIMARY KEY FOREIGN KEY references MarketPlants.Tipo(NIF),
+	Email NVARCHAR(255) FOREIGN KEY references MarketPlants.Tipo(Email),
 );
 
 CREATE TABLE MarketPlants.Pessoa(
@@ -31,52 +31,42 @@ CREATE TABLE MarketPlants.Pessoa(
     Genero NVARCHAR(255),
     NrTelefone INT,
 	Username NVARCHAR(255) FOREIGN KEY references MarketPlants.Utilizador(Username),
-	NIFUt INT PRIMARY KEY FOREIGN KEY references MarketPlants.Tipo(nif),
-	Email NVARCHAR(255) FOREIGN KEY references MarketPlants.Tipo(email),
+	NIF INT PRIMARY KEY FOREIGN KEY references MarketPlants.Tipo(NIF),
+	Email NVARCHAR(255) FOREIGN KEY references MarketPlants.Tipo(Email),
 );
 
 
 CREATE TABLE MarketPlants.Comprador(
-	ID_Comprador INT unique not null ,
+	ID_Comprador INT IDENTITY(1,1) Primary Key not null ,
 	DadosDePagamento NVARCHAR(255),
 	Username NVARCHAR(255) FOREIGN KEY references MarketPlants.Utilizador(Username),
-	Primary key(ID_Comprador)
+
 );
 
 CREATE TABLE MarketPlants.Vendedor(
-	ID_Vendedor INT unique not null ,
-	Rating int ,
+	ID_Vendedor INT IDENTITY(1,1) Primary key not null ,
+	Rating int not null ,
 	Username NVARCHAR(255) FOREIGN KEY references MarketPlants.Utilizador(Username),
 	CondicoesDeLevantamentoEntrega NVARCHAR(255),
-	Primary key(ID_Vendedor)
 );
 
 CREATE TABLE MarketPlants.EntidadeRegulamentadora(
-	ID_Entidade INT unique not null ,
-	nome NVARCHAR(255),
+	ID_Entidade INT IDENTITY(1,1) Primary key not null ,
+	nome NVARCHAR(255) not null,
 	ID_Vendedor INT FOREIGN KEY references MarketPlants.Vendedor(ID_Vendedor),
-	Primary key(ID_Entidade)
 );
 
 CREATE TABLE MarketPlants.Catalogo(
 	NrArtigos INT,
-	Promoção INT,
-	ID_Vendedor INT PRIMARY KEY FOREIGN KEY references MarketPlants.Vendedor(ID_Vendedor),
+	Promocao INT,
+	ID_Vendedor INT IDENTITY(1,1) PRIMARY KEY FOREIGN KEY references MarketPlants.Vendedor(ID_Vendedor),
 );
 
 CREATE TABLE MarketPlants.Artigo(
-	ID_Artigo INT PRIMARY KEY,
+	ID_Artigo INT IDENTITY(1,1) PRIMARY KEY,
 	Descricao NVARCHAR(255),
 	Preco INT ,
 	ID_Vendedor INT FOREIGN KEY references MarketPlants.Vendedor(ID_Vendedor),
-);
-
-CREATE TABLE MarketPlants.Carrinho(
-	NrArtigos INT,
-	PreçoTotal INT,
-	ID_Comprador INT PRIMARY KEY FOREIGN KEY references MarketPlants.Comprador(ID_Comprador), -- Needs to be added to the diagram
-	ID_Vendedor INT FOREIGN KEY references MarketPlants.Vendedor(ID_Vendedor), -- Needs to be added to the diagram
-	ID_Artigo INT FOREIGN KEY references MarketPlants.Artigo(ID_Artigo)
 );
 
 CREATE TABLE MarketPlants.Planta(
@@ -111,3 +101,18 @@ CREATE TABLE MarketPlants.Categoria(
 	Designacao NVARCHAR(255),
 	NomeCientificoPlanta NVARCHAR(255) FOREIGN KEY references MarketPlants.Planta(NomeCientifico)
 );
+
+CREATE TABLE MarketPlants.Carrinho(
+    ID_Carrinho INT IDENTITY(1,1) PRIMARY KEY,
+    ID_Comprador INT FOREIGN KEY REFERENCES MarketPlants.Comprador(ID_Comprador),
+    ID_Vendedor INT FOREIGN KEY REFERENCES MarketPlants.Vendedor(ID_Vendedor),
+    PrecoTotal INT,
+);
+
+CREATE TABLE MarketPlants.CarrinhoArtigo(
+    ID_Carrinho INT FOREIGN KEY REFERENCES MarketPlants.Carrinho(ID_Carrinho),
+    ID_Artigo INT FOREIGN KEY REFERENCES MarketPlants.Artigo(ID_Artigo),
+    Quantidade INT,
+    Preco INT ,
+);
+
